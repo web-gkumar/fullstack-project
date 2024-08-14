0945f6263cbe8ce0ad869ac3378a3d71a60fd732
web-gkumar <kgautam431@gmail.com>
2024-08-15 01:18:16 +0530

    api fixing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const {port} = process.env || 8000;
+const {port} = process.env;
 const routers = require('./server/routers/routers');
 const cors = require('cors');
 app.use(cors());
--- server/models/module_schema.js
+++ server/models/module_schema.js
@@ -2,28 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-
-const tablist = new Schema({
-    tabName : {type : String},
-    tabIcon : {type : String},
-    tabform : {type : String}
-});
-
-
-const menulistSchema = new Schema({
-    menuName : {type : String},
-    menuIcon : {type : String},
-    menuform : {type : String},
-    tablist : [tablist]
-});
-
 const setModuleSchema = new Schema({
     moduleName : { type : String, required : true},
     moduleIcon: { type: String},
     moduleform: { type: String},
-    menulist: [menulistSchema],
-    createdDate: { type: Date, default: Date.now}
-});
+}, { strict: false}, {timestamps: true}
+);
 
 
 
