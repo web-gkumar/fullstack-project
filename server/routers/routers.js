722add016c7cdc7e03190890e64b57cd9affb8ab
web-gkumar <kgautam431@gmail.com>
2025-01-21 12:57:09 +0530

    code backup

--- server/routers/routers.js
+++ server/routers/routers.js
@@ -4,7 +4,7 @@ const routers = express.Router();
 const { userSignUpValidation, userSignInValidation, verifyToken } = require('../utlity/userValidations');
 const { registerUser, loginUser, getUsers } = require('../controllers/auth_controller');
 const { createModule, getModules, getCurrentModule, updatModule, deleteModule } = require('../controllers/module_controller');
-const { createForm, getForms, getSingleForm, updatForm, deleteForm} = require('../controllers/forms_controller');
+const { createForm, getForms, getSingleForm, getFormbyId, updatForm, deleteForm} = require('../controllers/forms_controller');
 const { saveDyanimicFormsData, getByCollections, updateCollections, deleteGrid} = require('../controllers/collections_controller');
 const { fileController } = require('../controllers/file_controller');
 //Auth Api
@@ -20,7 +20,8 @@ routers.delete('/modules/:id', verifyToken, deleteModule);
 //Forms Api
 routers.post('/createForm', verifyToken, createForm);
 routers.get('/forms', verifyToken, getForms);
-routers.get('/forms/:formName', verifyToken, getSingleForm);
+routers.get('/forms/:id', verifyToken, getFormbyId);
+//routers.get('/forms/:formName', verifyToken, getSingleForm);
 routers.put('/forms/:id', verifyToken, updatForm);
 routers.delete('/forms/:id', verifyToken, deleteForm);
 //DynamicSaveForms Api
@@ -30,22 +31,22 @@ routers.put('/gridData/:id', verifyToken, updateCollections);
 routers.delete('/gridData/:id', verifyToken, deleteGrid);
 
 
-const multer = require('multer');
-const path = require('path');
-user.use(express.static(path.resolve(__dirname, 'public')));
-const storageFile = multer.diskStorage({
-    destination:(req,file,cb) => {
-        createGunzip(null, './public/uploaded')
-    },
-    filename:(req,file,cb) => {
-        cb(null,file.originalname);
-    }
-})
+// const multer = require('multer');
+// const path = require('path');
+// user.use(express.static(path.resolve(__dirname, 'public')));
+// const storageFile = multer.diskStorage({
+//     destination:(req,file,cb) => {
+//         createGunzip(null, './public/uploaded')
+//     },
+//     filename:(req,file,cb) => {
+//         cb(null,file.originalname);
+//     }
+// })
 
-const upload = multer({storage: storageFile})
+// const upload = multer({storage: storageFile})
 
 
-routers.post('/importUser', verifyToken, upload.single('file'), fileController)
+// routers.post('/importUser', verifyToken, upload.single('file'), fileController)
 
 
 module.exports = routers;
